cfba4fe133a6d1037fd5089f6c8a109e1b9ebcae
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-10-28 14:09:48 +0300

    feat: implement the homescreen hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
-
+"use client";
+import ExploreBtn from "@/components/ExploreBtn";
 
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <section>
+      <h1 className="text-center">The Hub for every Dev <br /> Event you can't miss</h1>
+      <p className="text-center mt-5">Hackathons, Meetups and Conferences, All in one place!</p>
+      
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <ul>
+          {
+            [1,2,3,4,5,6,7].map((item, index) => (
+              <li key={index}>{item}</li>
+            ))
+          }
+        </ul>
+      </div>
+    </section>
   )
 }
 
