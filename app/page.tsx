f241fec36b0627c01f96e33227d85c460e7470fb
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-11-23 15:35:31 +0300

    feat: add event details page with slug-based data fetching and notFound handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,36 @@
-// "use client";
 import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
-import { events } from "@/lib/constants"
-
+import {IEvent} from "@/database";
+import {cacheLife} from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
+const Page = async () => {
+    'use cache';
+    cacheLife('hours')
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
 
-const Home = async () => {
+    return (
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
 
-  const response = await fetch(`${BASE_URL}/api/events`)
+            <ExploreBtn />
 
-  const { events } = await response.json();
+            <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
 
-  return (
-    <section>
-      <h1 className="text-center">The Hub for every Dev <br /> Event you can&apos;t miss</h1>
-      <p className="text-center mt-5">Hackathons, Meetups and Conferences, All in one place!</p>
-      
-      <ExploreBtn />
-
-      <div className="mt-20 space-y-7 ">
-        <ul className="w-full grid grid-cols-3 gap-8">
-          {
-            events && events.length > 0 && events.map((event: IEvent) => (
-              <li key={event.title}>
-                <EventCard 
-                  {...event}
-                /> 
-              </li>
-            ))
-          }
-        </ul>
-      </div>
-    </section>
-  )
+                <ul className="events">
+                    {events && events.length > 0 && events.map((event: IEvent) => (
+                        <li key={event.title} className="list-none">
+                            <EventCard {...event} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    )
 }
 
-
-export default Home;
+export default Page;
