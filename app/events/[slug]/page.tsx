056d3cdb144f97d358465759ad95d2423e3c7dd3
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-11-24 13:42:58 +0300

    feat: implement full event details page with agenda, tags, and booking UI

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,7 +1,52 @@
 import { notFound } from 'next/navigation';
+import Image from "next/image";
+import BookEvent from "@/components/BookEvent";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
+
+const EventAgenda = ({agendaItems}: { agendaItems: string[]}) => (
+    <div className="agenda">
+        <h2>Agenda</h2>
+
+        <ul>
+            {
+                agendaItems.map((item) => (
+                    <li key={item}>
+                        {item}
+                    </li>
+                ))
+            }
+        </ul>
+    </div>
+)
+
+
+const EventDetailItem = ({icon, alt, label}: {icon: string; alt: string; label: string;}) => (
+    <div className="flex-row-gap-2 items-center">
+        <Image
+            src={icon}
+            alt={alt}
+            width={17}
+            height={17} 
+        />
+
+        <p>{label}</p>
+    </div>
+)
+
+
+const EventTags = ({ tags }: { tags : string[]}) => (
+    <div className="flex flex-row gap-1.5 flex-wrap">
+        {
+            tags.map((tag) => (
+                <div key={tag} className="pill">
+                    { tag }
+                </div>
+            ))
+        }
+    </div>
+)
 
 
 const EventDetailsPage = async({params} : {params: Promise<{slug: string}>}) => {
@@ -10,13 +55,103 @@ const EventDetailsPage = async({params} : {params: Promise<{slug: string}>}) => 
 
     const request = await fetch(`${BASE_URL}/api/events/${slug}`);
 
-    const { event } = await request.json();
+    const { event: { description, image, overview, date, time, location, mode, agenda, audience, tags, organizer} } = await request.json();
 
-    if(!event) return nofFound();
+    if(!description) return notFound();
+
+    const bookings = 10;
 
     return (
-        <section>
-            <h1>Event Details: {slug}</h1>
+        <section id="event">
+           <div className="header">
+                <h1>Event Description</h1>
+
+                <p>{ description }</p>
+           </div>
+
+           <div className="details">
+                {/* left side - event content */}
+
+                <article className="content">
+                    <Image 
+                        src={image}
+                        alt="Event Banner"
+                        width={800}
+                        height={800}
+                        className="banner"
+                    />
+
+                    <section className="flex-col-gap-2">
+                        <h2>Overview</h2>
+                        <p>{overview}</p>
+
+                        <EventDetailItem 
+                            icon="/icons/calender.svg"
+                            alt="calender"
+                            label={date}
+                        />
+
+                         <EventDetailItem 
+                            icon="/icons/clock.svg"
+                            alt="clock"
+                            label={time}
+                        />
+
+                         <EventDetailItem 
+                            icon="/icons/pin.svg"
+                            alt="pin"
+                            label={location}
+                        />
+
+                         <EventDetailItem 
+                            icon="/icons/mode.svg"
+                            alt="mode"
+                            label={mode}
+                        />
+
+                         <EventDetailItem 
+                            icon="/icons/audience.svg"
+                            alt="audience"
+                            label={audience}
+                        />
+                        
+                    </section>
+
+                    <EventAgenda 
+                        agendaItems={JSON.parse(agenda[0])}
+                    />
+
+                    <section className="flex-col-gap-2">
+                        <h2>About the Organizer</h2>
+                        <p>{organizer}</p>
+                    </section>
+
+                    <EventTags 
+                        tags={JSON.parse(tags[0])}
+                    />
+                </article>
+
+                {/* right side - booking form */}
+                <aside className="booking">
+                    <div className="signup-card">
+                        <h2>Book Your Spot</h2>
+
+                        {
+                            bookings > 0 ? (
+                                <p className="text-sm">
+                                    Join { bookings } people who have alsready booked their spot!
+                                </p>
+                            ) : (
+                                <p className="text-sm">
+                                    Be the first to book your spot!
+                                </p>
+                            )
+                        }
+
+                        <BookEvent />
+                    </div>
+                </aside>
+           </div>
         </section>
     )
 }
