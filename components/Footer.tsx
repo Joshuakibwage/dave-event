eb1349d9bedb3f4b7e56edd9b03005ac4e75cb1a
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-11-29 17:14:41 +0300

    fix: moved new Date() usage to the server layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 
-type FooterProps = {
-  year: number;
-}
 
-const Footer = ({ year }: FooterProps) => {
+
+const Footer = () => {
   return (
     <footer className="mt-12 w-full border-t border-neutral-900">
       <div className="flex justify-center items-center py-4 text-neutral-400">
-            &copy; {year} Dev Event
+            &copy; Dev Event
       </div>
     </footer>
   )
